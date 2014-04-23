7a2ff686f2574bd804978cadadbcc677135df789
aeverdeen <tucker.utah@gmail.com>
2014-04-22 22:39:20 -0400

    Initial Commit

---
+++ StepDX/Game.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace StepDX
+{
+    public partial class Game : Form
+    {
+        /// <summary>
+        /// The DirectX device we will draw on
+        /// </summary>
+        private Device device = null;
+
+        /// <summary> 
+        /// Height of our playing area (meters)
+        /// </summary>
+        private float playingH = 4;
+
+        /// <summary>
+        /// Width of our playing area (meters)
+        /// </summary>
+        private float playingW = 32;
+
+        /// <summary>
+        /// Vertex buffer for our drawing
+        /// </summary>
+        private VertexBuffer vertices = null;
+
+        /// <summary>
+        /// The background image class
+        /// </summary>
+        private Background background = null;
+
+        /// <summary>
+        /// All of the polygons that make up our world
+        /// </summary>
+        List<Polygon> world = new List<Polygon>();
+
+        /// <summary>
+        /// Our player sprite
+        /// </summary>
+        GameSprite player = new GameSprite();
+
+        /// <summary>
+        /// The collision testing subsystem
+        /// </summary>
+        Collision collision = new Collision();
+
+        /// <summary>
+        /// The sounds
+        /// </summary>
+        private GameSounds sounds;
+        
+        /// <summary>
+        /// What the last time reading was
+        /// </summary>
+        private long lastTime;
+
+        /// <summary>
+        /// A stopwatch to use to keep track of time
+        /// </summary>
+        private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        
+        /// <summary>
+        /// Initialize the Direct3D device for rendering
+        /// </summary>
+        /// <returns>true if successful</returns>
+        private bool InitializeDirect3D()
+        {
+            try
+            {
+                // Now let's setup our D3D stuff
+                PresentParameters presentParams = new PresentParameters();
+                presentParams.Windowed = true;
+                presentParams.SwapEffect = SwapEffect.Discard;
+
+                device = new Device(0, DeviceType.Hardware, this, CreateFlags.SoftwareVertexProcessing, presentParams);
+            }
+            catch (DirectXException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        //TODO: Add up and down buttons for movement
+        protected override void OnKeyDown(System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close(); // Esc was pressed
+            else if (e.KeyCode == Keys.Right)
+            {
+                Vector2 v = player.V;
+                v.X = 2.5f;
+                player.V = v;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                Vector2 v = player.V;
+                v.X = -2.5f;
+                player.V = v;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                Vector2 v = player.V;
+                v.Y = 2.5f;
+                player.V = v;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                Vector2 v = player.V;
+                v.Y = -2.5f;
+                player.V = v;
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                //TODO: Make the player shoot
+                sounds.Shoot();
+            }
+
+        }
+
+        //TODO: Make this work for up and down for the player as well
+        protected override void OnKeyUp(System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+            {
+                Vector2 v = player.V;
+                v.X = 0;
+                player.V = v;
+            }
+            else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                Vector2 v = player.V;
+                v.Y = 0;
+                player.V = v;
+            }
+        }
+
+        public Game()
+        {
+            InitializeComponent();
+            if (!InitializeDirect3D())
+                return;
+
+            vertices = new VertexBuffer(typeof(CustomVertex.PositionColored), // Type of vertex
+                                        4,      // How many
+                                        device, // What device
+                                        0,      // No special usage
+                                        CustomVertex.PositionColored.Format,
+                                        Pool.Managed);
+
+            background = new Background(device, playingW, playingH);
+            sounds = new GameSounds(this);
+
+            // Determine the last time
+            stopwatch.Start();
+            lastTime = stopwatch.ElapsedMilliseconds;
+
+            //TODO: Redo this so it uses the new player texture and shape
+            Texture spritetexture = TextureLoader.FromFile(device, "../../guy8.bmp");
+            player.Tex = spritetexture;
+            player.AddVertex(new Vector2(-0.2f, 0));
+            player.AddTex(new Vector2(0, 1));
+            player.AddVertex(new Vector2(-0.2f, 1));
+            player.AddTex(new Vector2(0, 0));
+            player.AddVertex(new Vector2(0.2f, 1));
+            player.AddTex(new Vector2(0.125f, 0));
+            player.AddVertex(new Vector2(0.2f, 0));
+            player.AddTex(new Vector2(0.125f, 1));
+            player.Color = Color.Transparent;
+            player.Transparent = true;
+            player.P = new Vector2(0.5f, 1);
+        }
+
+
+        //TODO: Need to render enemies at certain times. Don't know when, but probably need to put that here or in Advance
+        public void Render()
+        {
+            //This is the soundtrack. This can be changed if necessary
+            //sounds.Soundtrack();
+            if (device == null)
+                return;
+
+            device.Clear(ClearFlags.Target, System.Drawing.Color.Blue, 1.0f, 0);
+
+            int wid = Width;                            // Width of our display window
+            int hit = Height;                           // Height of our display window.
+            float aspect = (float)wid / (float)hit;     // What is the aspect ratio?
+
+            device.RenderState.ZBufferEnable = false;   // We'll not use this feature
+            device.RenderState.Lighting = false;        // Or this one...
+            device.RenderState.CullMode = Cull.None;    // Or this one...
+
+            float widP = playingH * aspect;         // Total width of window
+            
+            float winCenter = player.P.X;
+            if (winCenter - widP / 2 < 0)
+                winCenter = widP / 2;
+            else if (winCenter + widP / 2 > playingW)
+                winCenter = playingW - widP / 2;
+
+            device.Transform.Projection = Matrix.OrthoOffCenterLH(winCenter - widP / 2,
+                                                                  winCenter + widP / 2,
+                                                                  0, playingH, 0, 1);
+
+            //Begin the scene
+            device.BeginScene();
+
+            // Render the background
+            background.Render();
+
+            foreach (Polygon p in world)
+            {
+                p.Render(device);
+            }
+
+            player.Render(device);
+
+            //End the scene
+            device.EndScene();
+            device.Present();
+        }
+
+        /// <summary>
+        /// Advance the game in time
+        /// </summary>
+        public void Advance()
+        {
+            // How much time change has there been?
+            long time = stopwatch.ElapsedMilliseconds;
+            float delta = (time - lastTime) * 0.001f;       // Delta time in milliseconds
+            lastTime = time;
+
+            Vector2 q = player.V;
+            Vector2 r = player.P;
+
+            //These added borders, is there a better way to do this?
+            if (player.P.X < 0.1f)
+            {
+                q.X = 0;
+                r.X = 0.1f;
+            }
+            else if (player.P.X > 31.6f)
+            {
+                q.X = 0;
+                r.X = 31.6f;
+            }
+            if (player.P.Y < 0.1f)
+            {
+                q.Y = 0;
+                r.Y = 0.1f;
+            }
+            else if (player.P.Y > playingH - 0.1f)
+            {
+                q.Y = 0;
+                r.Y = playingH - 0.1f;
+            }
+
+            player.P = r;
+            player.V = q;
+            player.Advance(0);
+
+            while (delta > 0)
+            {
+                float step = delta;
+                if (step > 0.05f)
+                    step = 0.05f;
+
+                float maxspeed = Math.Max(Math.Abs(player.V.X), Math.Abs(player.V.Y));
+                if (maxspeed > 0)
+                {
+                    step = (float)Math.Min(step, 0.05 / maxspeed);
+                }
+
+                player.Advance(step);
+
+                foreach (Polygon p in world)
+                    p.Advance(step);
+
+                foreach (Polygon p in world)
+                {
+                    if (collision.Test(player, p))
+                    {
+                        float depth = collision.P1inP2 ?
+                                  collision.Depth : -collision.Depth;
+                        player.P = player.P + collision.N * depth;
+                        Vector2 v = player.V;
+                        if (collision.N.X != 0)
+                            v.X = 0;
+                        if (collision.N.Y != 0)
+                            v.Y = 0;
+                        player.V = v;
+                        player.Advance(0);
+                    }
+                }
+                
+                delta -= step;
+            }
+        }
+
+        public void AddObstacle(float left, float right, float bottom, float top, Color clr)
+        {
+            Polygon obs = new Polygon();
+            obs.AddVertex(new Vector2(left, top));
+            obs.AddVertex(new Vector2(right, top));
+            obs.AddVertex(new Vector2(right, bottom));
+            obs.AddVertex(new Vector2(left, bottom));
+            obs.Color = clr;
+            world.Add(obs);
+        }
+    }
+}
---
+++ StepDX/GameSounds.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectSound;
+
+namespace StepDX
+{
+    class GameSounds
+    {
+        private Device SoundDevice = null;
+
+        private SecondaryBuffer[] clank = new SecondaryBuffer[10];
+        int clankToUse = 0;
+
+        private SecondaryBuffer shoot = null;
+        private SecondaryBuffer soundtrack = null;
+
+        public GameSounds(Form form)
+        {
+            SoundDevice = new Device();
+            SoundDevice.SetCooperativeLevel(form, CooperativeLevel.Priority);
+
+            Load(ref shoot, "../../shoot.wav");
+            Load(ref soundtrack, "../../Frunk.wav");
+
+            for (int i = 0; i < clank.Length; i++)
+                Load(ref clank[i], "../../explosion.wav");
+        }
+
+        private void Load(ref SecondaryBuffer buffer, string filename)
+        {
+            try
+            {
+                buffer = new SecondaryBuffer(filename, SoundDevice);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unable to load " + filename,
+                                "Danger, Will Robinson", MessageBoxButtons.OK);
+                buffer = null;
+            }
+        }
+
+        public void Clank()
+        {
+            clankToUse = (clankToUse + 1) % clank.Length;
+
+            if (clank[clankToUse] == null)
+                return;
+
+            if (!clank[clankToUse].Status.Playing)
+                clank[clankToUse].Play(0, BufferPlayFlags.Default);
+        }
+
+        public void Shoot()
+        {
+            if (shoot == null)
+                return;
+
+            if (!shoot.Status.Playing)
+                shoot.Play(0, BufferPlayFlags.Default);
+        }
+
+        public void Soundtrack()
+        {
+            if (soundtrack == null)
+                return;
+
+            if (!soundtrack.Status.Playing)
+            {
+                soundtrack.SetCurrentPosition(0);
+                soundtrack.Play(0, BufferPlayFlags.Default);
+            }
+        }
+
+        public void SoundtrackEnd()
+        {
+            if (soundtrack == null)
+                return;
+
+            if (soundtrack.Status.Playing)
+                soundtrack.Stop();
+        }
+
+    }
+}
